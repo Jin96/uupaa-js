a62cb39e5818fc104dc31a8351cde0eefc819f32
uupaa.js <uupaa.js@694de1ec-184f-0410-bcc4-1b12f658377c>
2009-12-27 20:23:12 +0000

    uupaa.js ver0.7 alpha2

--- 0.6/build/build.php
+++
@@ -1,8 +0,0 @@
-<?php
-  // usage:
-  //  "build package"
-  require_once("misc.php");
-
-  $package = $argv[1];
-  buildScripts($package, "../uupaa.{$package}.js", 1);
-?>
--- 0.6/newsrc/uuMeta.canvas.setup.js
+++
@@ -1,1 +0,0 @@
-uuMeta.canvas.setup();
