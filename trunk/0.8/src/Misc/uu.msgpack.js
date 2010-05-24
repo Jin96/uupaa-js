8c6188c5a0dc5a7c780bf8a620f68a86609b1a4a
uupaa.js <uupaa.js@694de1ec-184f-0410-bcc4-1b12f658377c>
2010-05-24 19:07:09 +0000

    MessagePack encoder

--- trunk/0.8/src/Misc/uu.msgpack.js
+++ trunk/0.8/src/Misc/uu.msgpack.js
@@ -7,157 +7,123 @@
 (this.uu || this).msgpack || (function(namespace) {
 
 var _sign = { 8: 0x80, 16: 0x8000, 32: 0x80000000, 64: 0x8000000000000000 },
+    _bin2num = {}, // { "00000000": 0, "00000001": 1, ... }
     // --- minify ---
     _charCodeAt = "charCodeAt";
 
 namespace.msgpack = {
-    pack:   msgpackpack,    // uu.msgpack.pack(data:Mix, utf8:Boolean = false):ByteArray
-    unpack: msgpackunpack   // uu.msgpack.unpack(data:String/ByteArray, utf8:Boolean = false):Mix
+    pack:   msgpackpack,  // uu.msgpack.pack(data:Mix):ByteArray
+    unpack: msgpackunpack // uu.msgpack.unpack(data:String/ByteArray):Mix
 };
 
 // uu.msgpack.pack
-function msgpackpack(data,   // @param Mix:
-                     utf8) { // @param Boolean(= false): true is UTF8 raw data,
-                             //                          false is UCS2 raw data
+function msgpackpack(data) { // @param Mix:
                              // @return ByteArray:
-    return encode([], data, utf8);
+                             // @throws Error("OVERFLOW")
+    return encode([], data);
 }
 
-function encode(rv, mix, utf8) {
+// uu.msgpack.unpack
+function msgpackunpack(data) { // @param String/ByteArray:
+                               // @return Mix:
+    return { data: data, index: -1, byte: isArray(data),
+             decode: decode }.decode();
+}
+
+// inner - encoder
+function encode(rv,    // @param Array: result
+                mix) { // @param Mix: source data
     var size = 0, i = 0, ff = 0xff, rr;
 
     if (mix === null) {
         rv.push(0xc0);
-        return rv;
     } else if (isArray(mix)) {
         size = mix.length;
         setSize(rv, 16, size, [0x90, 0xdc, 0xdd]);
         for (; i < size; ++i) {
-            encode(rv, mix[i], utf8);
+            encode(rv, mix[i]);
         }
-        return rv;
-    }
-    switch (typeof mix) {
-    case "boolean":
-        rv.push(mix ? 0xc3 : 0xc2);
-        break;
-    case "number":
-        if (Math.floor(mix) === mix) { // bugfix // (mix | 0 == mix)
-            if (mix < 0) {
-                // int
-                if (mix >= -32) { // negative fixnum
-                    rv.push(0xe0 + mix + 32); // 0xff = -1, 0xe0 = -32
-                } else if (mix >= -0x100) {
-                    rv.push(0xd0, 0x100 + mix); // -64 = 0xc0
-                } else if (mix >= -0x10000) {
-                    mix += 0x10000;
-                    rv.push(0xd1, mix >> 8, mix & ff);
-                } else if (mix >= -0x100000000) {
-                    mix += 0x100000000;
-                    rv.push(0xd2, mix >>> 24, (mix >> 16) & ff, (mix >> 8) & ff, mix & ff);
-                } else {
-                    mix += 0x10000000000000000;
-                    rv.push(0xd3, (mix / 0x100000000000000) & ff,
-                                  (mix /   0x1000000000000) & ff,
-                                  (mix /     0x10000000000) & ff,
-                                  (mix /       0x100000000) & ff,
-                                  (mix >>> 24) & ff,
-                                  (mix >>  16) & ff,
-                                  (mix >>   8) & ff,
-                                           mix & ff);
-                }
-            } else {
-                // uint
-                if (mix < 128) {
-                    rv.push(mix); // positive fixnum
-                } else if (mix < 0x100) { // uint 8
-                    rv.push(0xcc, mix);
-                } else if (mix < 0x10000) { // uint 16
-                    rv.push(0xcd, mix >> 8, mix & ff);
-                } else if (mix < 0x100000000) { // uint 32
-                    rv.push(0xce, mix >>> 24, (mix >> 16) & ff, (mix >> 8) & ff, mix & ff);
-                } else {
-                    rv.push(0xcf, (mix / 0x100000000000000) & ff,
-                                  (mix /   0x1000000000000) & ff,
-                                  (mix /     0x10000000000) & ff,
-                                  (mix /       0x100000000) & ff,
-                                  (mix >>> 24) & ff,
-                                  (mix >>  16) & ff,
-                                  (mix >>   8) & ff,
-                                           mix & ff);
-                }
-            }
-        } else { // double
-//            return [0xcb,
-            // TODO
-        }
-        break;
-    case "string":
-        size = mix.length;
-        if (utf8) {
-            for (rr = [], i = 0; i < size; ++i) {
-                namespace.utf8.encode(mix.charAt(i), rr);
-            }
-            setSize(rv, 32, rr.length, [0xa0, 0xda, 0xdb]);
-            Array.prototype.push.apply(rv, rr);
-        } else {
-            setSize(rv, 32, size, [0xa0, 0xda, 0xdb]);
-            for (i = 0; i < size; ++i) {
-                rv.push(mix.charCodeAt(i)); // ucs2
-            }
-        }
-        break;
-    default: // hash
-        setSize(rv, 16, hashSize(mix), [0x80, 0xde, 0xdf]);
-        for (i in mix) {
-            encode(rv, i, utf8);
-            encode(rv, mix[i], utf8);
-        }
-    }
-    return rv;
-}
-
-function setSize(rv, fixSize, size, types) {
-    var ff = 0xff;
-
-    if (size < fixSize) {
-        rv.push(types[0] + size);
-    } else if (size < 0x10000) { // 16
-        rv.push(types[1], size >> 8, size & ff);
-    } else if (size < 0x100000000) { // 32
-        rv.push(types[2], size >>> 24, size >> 16 & ff, size >> 8 & ff, size & ff);
     } else {
-        throw new Error("OVERFLOW");
-    }
-}
+        switch (typeof mix) {
+        case "boolean":
+            rv.push(mix ? 0xc3 : 0xc2);
+            break;
+        case "number":
+            if (Math.floor(mix) === mix) { // bugfix // (mix | 0 == mix)
+                if (mix < 0) { // int
+                    if (mix >= -32) { // negative fixnum
+                        rv.push(0xe0 + mix + 32); // 0xff = -1, 0xe0 = -32
+                    } else if (mix >= -0x100) {
+                        rv.push(0xd0, mix);
+                    } else if (mix >= -0x10000) {
+                        rv.push(0xd1, mix >> 8, mix & ff);
+                    } else if (mix >= -0x100000000) {
+                        rv.push(0xd2, mix >>> 24, (mix >> 16) & ff, (mix >> 8) & ff, mix & ff);
+                    } else {
+                        mix += 0x10000000000000000;
+                        rv.push(0xd3, (mix / 0x100000000000000) & ff,
+                                      (mix /   0x1000000000000) & ff,
+                                      (mix /     0x10000000000) & ff,
+                                      (mix /       0x100000000) & ff,
+                                      (mix >>> 24) & ff,
+                                      (mix >>  16) & ff,
+                                      (mix >>   8) & ff,
+                                               mix & ff);
+                    }
+                } else { // uint
+                    if (mix < 128) {
+                        rv.push(mix); // positive fixnum
+                    } else if (mix < 0x100) { // uint 8
+                        rv.push(0xcc, mix);
+                    } else if (mix < 0x10000) { // uint 16
+                        rv.push(0xcd, mix >> 8, mix & ff);
+                    } else if (mix < 0x100000000) { // uint 32
+                        rv.push(0xce, mix >>> 24, (mix >> 16) & ff, (mix >> 8) & ff, mix & ff);
+                    } else {
+                        rv.push(0xcf, (mix / 0x100000000000000) & ff,
+                                      (mix /   0x1000000000000) & ff,
+                                      (mix /     0x10000000000) & ff,
+                                      (mix /       0x100000000) & ff,
+                                      (mix >>> 24) & ff,
+                                      (mix >>  16) & ff,
+                                      (mix >>   8) & ff,
+                                               mix & ff);
+                    }
+                }
+            } else { // double
+                rv = toIEEE754(mix);
+            }
+            break;
+        case "string":
+            size = mix.length;
 
-function hashSize(hash) {
-    var rv = 0;
-
-    if (Object.keys) {
-        return Object.keys(hash).length;
-    }
-    for (i in hash) {
-        hash.hasOwnProperty(i) && ++rv;
+//          if (utf8) {
+                for (rr = [], i = 0; i < size; ++i) {
+                    namespace.utf8.encode(mix.charAt(i), rr);
+                }
+                setSize(rv, 32, rr.length, [0xa0, 0xda, 0xdb]);
+                Array.prototype.push.apply(rv, rr);
+//          } else { // ucs2(unicode)
+//              setSize(rv, 32, size, [0xa0, 0xda, 0xdb]);
+//              for (i = 0; i < size; ++i) {
+//                  rv.push(mix.charCodeAt(i)); // ucs2
+//              }
+//          }
+            break;
+        default: // hash
+            setSize(rv, 16, hashSize(mix), [0x80, 0xde, 0xdf]);
+            for (i in mix) {
+                encode(rv, i);
+                encode(rv, mix[i]);
+            }
+        }
     }
     return rv;
 }
 
-function isArray(mix) {
-    return Object.prototype.toString.call(mix) === "[object Array]";
-}
-
-// uu.msgpack.unpack
-function msgpackunpack(data,   // @param String/ByteArray:
-                       utf8) { // @param Boolean(= false): true is UTF8 raw data,
-                               //                          false is UCS2 raw data
-                               // @return Mix:
-    return { data: data, index: -1, utf8: utf8, byte: isArray(data),
-             decode: decode }.decode();
-}
-
+// inner - decoder
 function decode() { // @return Mix:
-    var rv = 0, size = 0, i = 0, msb = 0, exp, key,
+    var rv = 0, size = 0, i = 0, msb = 0, exp, key, nv,
         that = this,
         data = that.data,
         type = that.byte ? data[++that.index]
@@ -193,20 +159,21 @@ function decode() { // @return Mix:
     case 0xce:  return readByte(that, 4);    // uint 32
     case 0xcd:  return readByte(that, 2);    // uint 16
     case 0xcc:  return readByte(that, 1);    // uint 8
-    case 0xd3:  rv = readByte(that, 8);          // int 64
-    case 0xd2:  rv || (rv = readByte(that, 4));  // int 32
-    case 0xd1:  rv || (rv = readByte(that, 2));  // int 16
-    case 0xd0:  rv || (rv = readByte(that, 1));  // int 8
+    case 0xd3:  nv = readByte(that, 8);          // int 64
+    case 0xd2:  nv === void 0 && (nv = readByte(that, 4));  // int 32
+    case 0xd1:  nv === void 0 && (nv = readByte(that, 2));  // int 16
+    case 0xd0:  nv === void 0 && (nv = readByte(that, 1));  // int 8
                 msb = 4 << ((type & 0x3) + 1); // 8, 16, 32, 64
-                return rv < _sign[msb] ? rv : rv - _sign[msb] * 2;
+                return nv < _sign[msb] ? nv : nv - _sign[msb] * 2;
     case 0xdb:  size = readByte(that, 4);           // raw 32
     case 0xda:  size || (size = readByte(that, 2)); // raw 16
     case 0xa0:  for (rv = []; i < size; ++i) {      // raw
                     rv[i] = that.byte ? data[++that.index]
                                       : data[_charCodeAt](++that.index);
                 }
-                return that.utf8 ? namespace.utf8.decode(rv)
-                                 : String.fromCharCode.apply(null, rv);
+//              return utf8 ? namespace.utf8.decode(rv)
+//                          : String.fromCharCode.apply(null, rv);
+                return namespace.utf8.decode(rv);
     case 0xdf:  size = readByte(that, 4);           // map 32
     case 0xde:  size || (size = readByte(that, 2)); // map 16
     case 0x80:  for (rv = {}; i < size; ++i) {      // map
@@ -224,6 +191,7 @@ function decode() { // @return Mix:
     return;
 }
 
+// inner - read byte
 function readByte(that,   // @param Object:
                   size) { // @param Number:
                           // @return Number:
@@ -243,5 +211,97 @@ function readByte(that,   // @param Object:
     that.index = i;
     return rv;
 }
+
+// inner - Number to IEEE754 formated ByteArray
+function toIEEE754(num) { // @param Number:
+                          // @return ByteArray: [0xcb, ...]
+
+    // see -> http://pc.nikkeibp.co.jp/pc21/special/gosa/eg4.shtml
+    var sign = num < 0 ? "1" : "0";
+    var fraction = num.toString(2).slice(1);
+    //  (123.456).toString(2)
+    //      = "1111011.0111010010111100011010100111111011111001110111"
+    //                          v
+    //  slice(1)
+    //      =  "111011.0111010010111100011010100111111011111001110111"
+    var exp;
+
+    if (fraction.charAt(0) === ".") {
+        // 0.1 -> ".0001100110011001100110011001100110011001100110011001101"
+        exp = 1023 - fraction.indexOf("1");
+        exp = ("00000000000" + exp.toString(2)).slice(-11); // pad zero
+
+        fraction = fraction.replace(/^\.0*1/, "");
+    } else {
+        // 123.345 -> "111011.0111010010111100011010100111111011111001110111"
+        exp = 1023 + fraction.indexOf(".");
+        exp = ("00000000000" + exp.toString(2)).slice(-11); // pad zero
+
+        fraction = fraction.replace(/\./, "");
+        //  replace(/\./, "")
+        //      =  "11110110111010010111100011010100111111011111001110111"
+    }
+    fraction += "0000000000000000000000000000000000000000000000000000"; // x52
+
+
+    // TODO: Rounding modes
+
+
+    var s = (sign + exp + fraction).slice(0, 64);
+
+    return [0xcb, _bin2num[s.slice(0,  8) ], _bin2num[s.slice(8,  16)],
+                  _bin2num[s.slice(16, 24)], _bin2num[s.slice(24, 32)],
+                  _bin2num[s.slice(32, 40)], _bin2num[s.slice(40, 48)],
+                  _bin2num[s.slice(48, 56)], _bin2num[s.slice(56, 64)]];
+}
+
+// inner - set size
+function setSize(rv,      // @param Array: result
+                 fixSize, // @param Number: fix size. eg: 16 or 32
+                 size,    // @param Number: size
+                 types) { // @param Array: type formats. eg: [0x90, 0xdc, 0xdd]
+    var ff = 0xff;
+
+    if (size < fixSize) {
+        rv.push(types[0] + size);
+    } else if (size < 0x10000) { // 16
+        rv.push(types[1], size >> 8, size & ff);
+    } else if (size < 0x100000000) { // 32
+        rv.push(types[2], size >>> 24, size >> 16 & ff, size >> 8 & ff, size & ff);
+    } else {
+        throw new Error("OVERFLOW");
+    }
+}
+
+// inner - get hash size
+function hashSize(hash) { // @param Hash:
+                          // @return Number:
+    var rv = 0;
+
+    if (Object.keys) {
+        return Object.keys(hash).length;
+    }
+    for (i in hash) {
+        hash.hasOwnProperty(i) && ++rv;
+    }
+    return rv;
+}
+
+// inner - Array.isArray alias
+function isArray(mix) { // @param Mix:
+                        // @return Boolean:
+    return Object.prototype.toString.call(mix) === "[object Array]";
+}
+
+// --- init ---
+// inner - create binaryToNumber hash
+(function() {
+    var i = 0, s;
+
+    for (; i < 256; ++i) {
+        s = ("0000000" + i.toString(2)).slice(-8);
+        _bin2num[s] = i;
+    }
+})();
 
 })(this.uu || this);
--- trunk/0.8/src/uu.test.js
+++ trunk/0.8/src/uu.test.js
@@ -28,47 +28,52 @@
 
 uu.test || (function(uu) {
 
-uu.test = uutest; // uu.test(testCase:Hash, throwsError:Boolean = false)
-uu.test.addType = uutestaddtype; // uu.test.addType(operator:String, callback:Function):Boolean
+uu.test = uutest;                   // uu.test(testCase:Hash, throwsError:Boolean = false)
+uu.test.addType     = addType;      // uu.test.addType(operator:String, callback:Function):Boolean
+uu.test.toHexString = toHexString;  // uu.test.toHexString(source:ByteArray):String
+uu.test.data = { index: 0, ok: 0, ng: 0, total: 0, run: 0 };
 
 // uu.test
 function uutest(testCase,      // @param Hash: { title: evaluator, ... }
                 throwsError) { // @param Boolean(= false): true is throws
-    var rv, title, index = 0, i, iz,
-        ok = 0, total = 0, ng = [], br = /\n/g,
-        info, ngzone, ol;
+    var rv, data = uu.test.data, title, ngzone, ol;
 
-    uu.node.add(
-        info = uu.div("class,uutestinfo",
-            ngzone = uu.div("class,ngzone")));
-    uu.node.add(
-        uu.div("class,uutest",
-            ol = uu.ol()));
+    if (!data.run++) {
+        uu.node.add(uu.div("class,uutestinfo",
+                        uu.div("class,ngzone"),
+                            uu.div("class,score", "0 / 0")));
+        uu.node.add(uu.div("class,uutest", uu.ol()));
+    }
+    ngzone = uu.klass("ngzone")[0];
+    ol = uu.klass("uutest")[0].firstChild;
 
     for (title in testCase) {
-        ++index;
+        ++data.index;
 
         if (uu.isFunction(testCase[title])) {
             rv = question(title, testCase[title], throwsError);
-            rv[0] === 1 ? ++ok : ng.push(index);
-            ++total;
+            if (rv[0] === 1) {
+                ++data.ok;
+            } else {
+                ++data.ng;
+                uu.node.add(uu.a("href,#uutest" + data.index, data.index + ""), ngzone);
+            }
+            ++data.total;
         } else {
             rv = [3, ""]; // { title: "" }
         }
         uu.node.add(
             uu.li("class,li" + rv[0], // .li0, .li1, .li2, .li3
                 uu.p("class,p",
-                    uu.a("name,uutest" + index,
-                        uu.text(title.replace(br, "<br />"))),
+                    uu.a("name,uutest" + data.index,
+                        uu.text(title.replace(/\n/g, "<br />"))),
                     uu.br(),
-                    uu.text(rv[1]))), ol);
+                    uu.text(rv.slice(1).join(" // ")))), ol);
     }
-    for (i = 0, iz = ng.length; i < iz; ++i) {
-        uu.node.add(uu.a("href,#uutest" + ng[i], ng[i] + ""), ngzone);
-    }
-    iz && (ol.style.backgroundColor = "red");
+    // RED BGCOLOR
+    ol.style.backgroundColor = data.ng ? "red" : "";
 
-    uu.node.add(uu.div("class,score", uu.format("?? / ??", ok, total)), info);
+    uu.text(uu.klass("score")[0], "?? / ??", data.ok, data.total);
 }
 uutest.type = {}; // { operator: callback, ... }
 uutest.trimOperator = /NOT|!|IS/ig;
@@ -76,7 +81,7 @@ uutest.trimOperator = /NOT|!|IS/ig;
 function question(title,         // @param String:
                   evaluator,     // @param Function:
                   throwsError) { // @param Boolean:
-                                 // @return Array: [state, msg]
+                                 // @return Array: [state, msg, ...]
                                  //    state - Number: 0 is false, 1 is true, 2 is error
                                  //    msg - String:
     var rv, param, result, callback, lhs, rhs;
@@ -96,8 +101,11 @@ function question(title,         // @param String:
 
             rv = [result, uu.format("?? ?? ??", lhs, param[1], rhs)];
 
-            callback = param[3] || param[2];
-            uu.isFunction(callback) && callback(); // after callback
+            // after callback
+            param.slice(param[2] === void 0 ? 2 : 3).forEach(function(v, i) {
+                uu.isFunction(v) ? v() // after callback
+                                 : rv.push(v);
+            });
         }
     } catch(err) {
         if (throwsError) {
@@ -161,12 +169,24 @@ function judge(lhs,      // @param Mix: left hand set
 }
 
 // uu.test.addType - register type detective operator
-function uutestaddtype(operator,   // @param String:
-                       callback) { // @param Function:
-                                   // @return Boolean:
+function addType(operator,   // @param String:
+                 callback) { // @param Function:
+                             // @return Boolean:
     uutest.type[operator.toUpperCase().
                          replace(/\s+/g, "").
                          replace(uutest.trimOperator, "")] = callback;
+}
+
+// uu.test.toHexString - array to HexString
+function toHexString(source) { // @param ByteArray:
+                               // @return String: "[0xnn, 0xnn, ... ]"
+    var rv = [], v, i = 0, iz = source.length;
+
+    for (; i < iz; ++i) {
+        v = source[i];
+        rv.push("0x" + uu.hash.num2hh[v * (v < 0 ? -1 : 1)]);
+    }
+    return "[" + rv.join(", ") + "]";
 }
 
 })(uu);
