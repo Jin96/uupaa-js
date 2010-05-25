aefb833cc15af6040c75e409b928ffb37c4b8003
uupaa.js <uupaa.js@694de1ec-184f-0410-bcc4-1b12f658377c>
2010-05-25 06:16:17 +0000

    fix bug

--- 0.8/src/Misc/uu.msgpack.js
+++ 0.8/src/Misc/uu.msgpack.js
@@ -31,15 +31,15 @@ function msgpackunpack(data) { // @param String/ByteArray:
 }
 
 // inner - encoder
-function encode(rv,    // @param Array: result
+function encode(rv,    // @param ByteArray: result
                 mix) { // @param Mix: source data
-    var size = 0, i = 0, ff = 0xff, rr;
+    var size = 0, i = 0, rr;
 
-    if (mix === null) {
+    if (mix == null) { // null or undefined
         rv.push(0xc0);
     } else if (isArray(mix)) {
         size = mix.length;
-        setSize(rv, 16, size, [0x90, 0xdc, 0xdd]);
+        setType(rv, 16, size, [0x90, 0xdc, 0xdd]);
         for (; i < size; ++i) {
             encode(rv, mix[i]);
         }
@@ -49,50 +49,7 @@ function encode(rv,    // @param Array: result
             rv.push(mix ? 0xc3 : 0xc2);
             break;
         case "number":
-            if (Math.floor(mix) === mix) { // bugfix // (mix | 0 == mix)
-                if (mix < 0) { // int
-                    if (mix >= -32) { // negative fixnum
-                        rv.push(0xe0 + mix + 32); // 0xff = -1, 0xe0 = -32
-                    } else if (mix >= -0x100) {
-                        rv.push(0xd0, mix);
-                    } else if (mix >= -0x10000) {
-                        rv.push(0xd1, mix >> 8, mix & ff);
-                    } else if (mix >= -0x100000000) {
-                        rv.push(0xd2, mix >>> 24, (mix >> 16) & ff, (mix >> 8) & ff, mix & ff);
-                    } else {
-                        mix += 0x10000000000000000;
-                        rv.push(0xd3, (mix / 0x100000000000000) & ff,
-                                      (mix /   0x1000000000000) & ff,
-                                      (mix /     0x10000000000) & ff,
-                                      (mix /       0x100000000) & ff,
-                                      (mix >>> 24) & ff,
-                                      (mix >>  16) & ff,
-                                      (mix >>   8) & ff,
-                                               mix & ff);
-                    }
-                } else { // uint
-                    if (mix < 128) {
-                        rv.push(mix); // positive fixnum
-                    } else if (mix < 0x100) { // uint 8
-                        rv.push(0xcc, mix);
-                    } else if (mix < 0x10000) { // uint 16
-                        rv.push(0xcd, mix >> 8, mix & ff);
-                    } else if (mix < 0x100000000) { // uint 32
-                        rv.push(0xce, mix >>> 24, (mix >> 16) & ff, (mix >> 8) & ff, mix & ff);
-                    } else {
-                        rv.push(0xcf, (mix / 0x100000000000000) & ff,
-                                      (mix /   0x1000000000000) & ff,
-                                      (mix /     0x10000000000) & ff,
-                                      (mix /       0x100000000) & ff,
-                                      (mix >>> 24) & ff,
-                                      (mix >>  16) & ff,
-                                      (mix >>   8) & ff,
-                                               mix & ff);
-                    }
-                }
-            } else { // double
-                rv = toIEEE754(mix);
-            }
+            encodeNumber(rv, mix);
             break;
         case "string":
             size = mix.length;
@@ -101,17 +58,17 @@ function encode(rv,    // @param Array: result
                 for (rr = [], i = 0; i < size; ++i) {
                     namespace.utf8.encode(mix.charAt(i), rr);
                 }
-                setSize(rv, 32, rr.length, [0xa0, 0xda, 0xdb]);
+                setType(rv, 32, rr.length, [0xa0, 0xda, 0xdb]);
                 Array.prototype.push.apply(rv, rr);
 //          } else { // ucs2(unicode)
-//              setSize(rv, 32, size, [0xa0, 0xda, 0xdb]);
+//              setType(rv, 32, size, [0xa0, 0xda, 0xdb]);
 //              for (i = 0; i < size; ++i) {
 //                  rv.push(mix.charCodeAt(i)); // ucs2
 //              }
 //          }
             break;
         default: // hash
-            setSize(rv, 16, hashSize(mix), [0x80, 0xde, 0xdf]);
+            setType(rv, 16, hashSize(mix), [0x80, 0xde, 0xdf]);
             for (i in mix) {
                 encode(rv, i);
                 encode(rv, mix[i]);
@@ -151,7 +108,7 @@ function decode() { // @return Mix:
                 return (rv & _sign[32] ? -1 : 1)
                         * (rv & 0x7fffff | 0x800000) * Math.pow(2, exp - 23);
     case 0xcb:  rv = readByte(that, 4);      // double
-                exp = ((rv >> 20) & 0x7ff) - 1023; // +1023 bias
+                exp = ((rv >> 20) & 0x7ff) - 1023; // exp bias
                 return (rv & _sign[32] ? -1 : 1)
                         * ((rv & 0xfffff | 0x100000) * Math.pow(2, exp - 20)
                             + readByte(that, 4) * Math.pow(2, exp - 52));
@@ -159,7 +116,7 @@ function decode() { // @return Mix:
     case 0xce:  return readByte(that, 4);    // uint 32
     case 0xcd:  return readByte(that, 2);    // uint 16
     case 0xcc:  return readByte(that, 1);    // uint 8
-    case 0xd3:  nv = readByte(that, 8);          // int 64
+    case 0xd3:  nv = readByte(that, 8);                     // int 64
     case 0xd2:  nv === void 0 && (nv = readByte(that, 4));  // int 32
     case 0xd1:  nv === void 0 && (nv = readByte(that, 2));  // int 16
     case 0xd0:  nv === void 0 && (nv = readByte(that, 1));  // int 8
@@ -202,7 +159,7 @@ function readByte(that,   // @param Object:
             rv += (byte ? data[++i] : data[_charCodeAt](++i)) *   0x1000000000000; // << 48
             rv += (byte ? data[++i] : data[_charCodeAt](++i)) *     0x10000000000; // << 40
             rv += (byte ? data[++i] : data[_charCodeAt](++i)) *       0x100000000; // << 32
-    case 4: rv += (byte ? data[++i] : data[_charCodeAt](++i)) *         0x1000000; // << 24 (keep msb)
+    case 4: rv += (byte ? data[++i] : data[_charCodeAt](++i)) *         0x1000000; // << 24 (msb)
             rv += (byte ? data[++i] : data[_charCodeAt](++i)) *           0x10000; // << 16
     case 2: rv += (byte ? data[++i] : data[_charCodeAt](++i)) *             0x100; // << 8
     case 1: rv += (byte ? data[++i] : data[_charCodeAt](++i));
@@ -212,34 +169,87 @@ function readByte(that,   // @param Object:
     return rv;
 }
 
+// inner - encode number
+function encodeNumber(rv,    // @param ByteArray: result
+                      mix) { // @param Number:
+    if (Math.floor(mix) === mix) { // bugfix // (mix | 0 == mix)
+        if (mix < 0) { // int
+            if (mix >= -32) { // negative fixnum
+                rv.push(0xe0 + mix + 32); // 0xff = -1, 0xe0 = -32
+            } else if (mix >= -0x100) {
+                rv.push(0xd0, mix);
+            } else if (mix >= -0x10000) {
+                rv.push(0xd1, mix >> 8, mix & 0xff);
+            } else if (mix >= -0x100000000) {
+                rv.push(0xd2, mix >>> 24, (mix >> 16) & 0xff,
+                                          (mix >>  8) & 0xff, mix & 0xff);
+            } else {
+                rv.push(0xd3, (mix / 0x100000000000000) & 0xff,
+                              (mix /   0x1000000000000) & 0xff,
+                              (mix /     0x10000000000) & 0xff,
+                              (mix /       0x100000000) & 0xff,
+                              (mix >>> 24) & 0xff,
+                              (mix >>  16) & 0xff,
+                              (mix >>   8) & 0xff,
+                                       mix & 0xff);
+            }
+        } else { // uint
+            if (mix < 128) {
+                rv.push(mix); // positive fixnum
+            } else if (mix < 0x100) { // uint 8
+                rv.push(0xcc, mix);
+            } else if (mix < 0x10000) { // uint 16
+                rv.push(0xcd, mix >> 8, mix & 0xff);
+            } else if (mix < 0x100000000) { // uint 32
+                rv.push(0xce, mix >>> 24, (mix >> 16) & 0xff,
+                                          (mix >>  8) & 0xff, mix & 0xff);
+            } else {
+                rv.push(0xcf, (mix / 0x100000000000000) & 0xff,
+                              (mix /   0x1000000000000) & 0xff,
+                              (mix /     0x10000000000) & 0xff,
+                              (mix /       0x100000000) & 0xff,
+                              (mix >>> 24) & 0xff,
+                              (mix >>  16) & 0xff,
+                              (mix >>   8) & 0xff,
+                                       mix & 0xff);
+            }
+        }
+    } else { // double
+        toIEEE754(rv, mix);
+    }
+}
+
 // inner - Number to IEEE754 formated ByteArray
-function toIEEE754(num) { // @param Number:
-                          // @return ByteArray: [0xcb, ...]
+function toIEEE754(rv,    // @param ByteArray: result, [0xcb, ...]
+                   num) { // @param Number:
 
     // see -> http://pc.nikkeibp.co.jp/pc21/special/gosa/eg4.shtml
+
     var sign = num < 0 ? "1" : "0";
-    var fraction = num.toString(2).slice(1);
+    var fraction = Math.abs(num).toString(2);
     //  (123.456).toString(2)
     //      = "1111011.0111010010111100011010100111111011111001110111"
-    //                          v
-    //  slice(1)
-    //      =  "111011.0111010010111100011010100111111011111001110111"
+    //  (0.1).toString(2)
+    //      = "0.0001100110011001100110011001100110011001100110011001101"
     var exp;
 
-    if (fraction.charAt(0) === ".") {
-        // 0.1 -> ".0001100110011001100110011001100110011001100110011001101"
-        exp = 1023 - fraction.indexOf("1");
-        exp = ("00000000000" + exp.toString(2)).slice(-11); // pad zero
+    if (fraction.charAt(1) === ".") {
+        if (fraction.charAt(0) === "1") { // /^1\./
+            exp = 1023;
+            exp = "0" + exp.toString(2); // pad zero
 
-        fraction = fraction.replace(/^\.0*1/, "");
+            fraction = fraction.slice(2); // "1.xxx" -> "xxx"
+        } else { // /^0\./
+            exp = 1023 - fraction.indexOf("1") + 1; // "0.00011000..." -> -5 + 1 = 4
+            exp = ("00000000000" + exp.toString(2)).slice(-11); // pad zero
+
+            fraction = fraction.slice(6); // "0.00011000..." -> "1000..."
+        }
     } else {
-        // 123.345 -> "111011.0111010010111100011010100111111011111001110111"
-        exp = 1023 + fraction.indexOf(".");
+        exp = 1023 + fraction.indexOf(".") - 1;
         exp = ("00000000000" + exp.toString(2)).slice(-11); // pad zero
 
-        fraction = fraction.replace(/\./, "");
-        //  replace(/\./, "")
-        //      =  "11110110111010010111100011010100111111011111001110111"
+        fraction = fraction.slice(1).replace(/\./, "");
     }
     fraction += "0000000000000000000000000000000000000000000000000000"; // x52
 
@@ -249,25 +259,24 @@ function toIEEE754(num) { // @param Number:
 
     var s = (sign + exp + fraction).slice(0, 64);
 
-    return [0xcb, _bin2num[s.slice(0,  8) ], _bin2num[s.slice(8,  16)],
+    rv.push(0xcb, _bin2num[s.slice(0,  8) ], _bin2num[s.slice(8,  16)],
                   _bin2num[s.slice(16, 24)], _bin2num[s.slice(24, 32)],
                   _bin2num[s.slice(32, 40)], _bin2num[s.slice(40, 48)],
-                  _bin2num[s.slice(48, 56)], _bin2num[s.slice(56, 64)]];
+                  _bin2num[s.slice(48, 56)], _bin2num[s.slice(56, 64)]);
 }
 
-// inner - set size
-function setSize(rv,      // @param Array: result
+// inner - set type and fixed size
+function setType(rv,      // @param ByteArray: result
                  fixSize, // @param Number: fix size. eg: 16 or 32
                  size,    // @param Number: size
-                 types) { // @param Array: type formats. eg: [0x90, 0xdc, 0xdd]
-    var ff = 0xff;
-
+                 types) { // @param ByteArray: type formats. eg: [0x90, 0xdc, 0xdd]
     if (size < fixSize) {
         rv.push(types[0] + size);
     } else if (size < 0x10000) { // 16
-        rv.push(types[1], size >> 8, size & ff);
+        rv.push(types[1], size >> 8, size & 0xff);
     } else if (size < 0x100000000) { // 32
-        rv.push(types[2], size >>> 24, size >> 16 & ff, size >> 8 & ff, size & ff);
+        rv.push(types[2], size >>> 24, (size >> 16) & 0xff,
+                                       (size >>  8) & 0xff, size & 0xff);
     } else {
         throw new Error("OVERFLOW");
     }
