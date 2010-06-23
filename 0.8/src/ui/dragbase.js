200b7d4bab827b9e583fcff3c9c83a6580dd13fa
uupaa.js <uupaa.js@694de1ec-184f-0410-bcc4-1b12f658377c>
2010-06-23 16:10:04 +0000

    add uu.ver.touch iPhone MultiTouch support

--- 0.8/src/ui/dragbase.js
+++ 0.8/src/ui/dragbase.js
@@ -9,7 +9,8 @@ uu.ui.dragbase || (function(uu) {
 
 uu.ui.dragbase = uuuidragbase; // drag & drop base handler
 
-var _uuuidrag = "data-uuuidrag"; // node["data-uuuidrag"] = { dragging: Boolean, x, y }
+var _uuuidrag = "data-uuuidrag", // node["data-uuuidrag"] = { dragging: Boolean, x, y }
+    _touch = uu.ver.touch;
 
 // uu.ui.dragbase -
 function uuuidragbase(evt,      // @param event:
@@ -24,13 +25,22 @@ function uuuidragbase(evt,      // @param event:
         pageX = evt.pageX,
         pageY = evt.pageY,
         code  = evt.code,
-        dragInfo = grip[_uuuidrag] || {};
+        dragInfo = grip[_uuuidrag] || {},
+        touches, finger, identifier, i, iz; // for iPhone
 
-    if (code === 1 && !dragInfo.dragging) { // mousedown
+    if (code === 1 && !dragInfo.dragging) { // 1: mousedown, touchstart
+
+        if (_touch) {
+            finger = evt.touches[evt.touches.length - 1];
+            identifier = finger.identifier;
+            pageX = finger.pageX;
+            pageY = finger.pageY;
+        }
 
         dragInfo = grip[_uuuidrag] = {
             x: pageX - (parseInt(node.style.left) || 0),
             y: pageY - (parseInt(node.style.top)  || 0),
+            id: identifier, // touch.identifier
             dragging: 1
         };
 
@@ -38,7 +48,7 @@ function uuuidragbase(evt,      // @param event:
 
         uu.ui.zindex.beginDrag(node);
 
-    } else if (code === 2 && dragInfo.dragging) { // mouseup
+    } else if (code === 2 && dragInfo.dragging) { // 2: mouseup, touchend
 
         dragInfo.dragging = 0;
 
@@ -46,7 +56,18 @@ function uuuidragbase(evt,      // @param event:
 
         uu.ui.zindex.endDrag(node);
 
-    } else if (code === 3 && dragInfo.dragging) { // mousemove
+    } else if (code === 3 && dragInfo.dragging) { // 3: mousemove, touchmove
+
+        if (_touch) {
+            touches = evt.touches;
+            for (i = 0, iz = touches.length; i < iz; ++i) {
+                finger = touches[i];
+                if (dragInfo.id === finger.identifier) {
+                    pageX = finger.pageX;
+                    pageY = finger.pageY;
+                }
+            }
+        }
 
         node.style.left = (pageX - dragInfo.x) + "px";
         node.style.top  = (pageY - dragInfo.y) + "px";
